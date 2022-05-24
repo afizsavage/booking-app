e987c30fa6c9c00eb5c989ca02ab995f57e89669
cwaku <rickymormor1@gmail.com>
2022-05-24 18:59:32 +0000

    Use RTK Query to fetch from API

--- src/components/reserve.js
+++ src/components/reserve.js
@@ -1,3 +1,24 @@
-const Reserve = () => <h1>Reserve</h1>;
+import { useSelector } from 'react-redux';
+import { selectAvailableScooterIds, useGetAvailableScotersQuery } from '../redux/reservations/reservationsSlice';
+
+const Reserve = () => {
+  const { isLoading, isSuccess, isError } = useGetAvailableScotersQuery();
+
+  const orderedScootersIds = useSelector(selectAvailableScooterIds);
+  // const scootersStatus = useSelector(getAvailableScootersStatus);
+
+  let content = null;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  if (isError) {
+    content = <p>Error!</p>;
+  }
+  if (isSuccess) {
+    content = orderedScootersIds.map((id) => <p key={id}>{id}</p>);
+  }
+
+  return <div>{content}</div>;
+};
 
 export default Reserve;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,11 +3,15 @@ import logger from 'redux-logger';
 import { configureStore } from '@reduxjs/toolkit';
 import bikesReducer from './bikes/bikesSlice';
 import userReducer from './users/userSlice';
+import reservationsApi from '../services/reservationsApi';
 
 export default configureStore({
   reducer: {
+    [reservationsApi.reducerPath]: reservationsApi.reducer,
     bikes: bikesReducer,
     user: userReducer,
   },
-  middleware: [thunk, logger],
+  // middleware: [thunk, logger],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(reservationsApi.middleware, thunk, logger),
 });
