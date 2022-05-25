cb17bf80fa13f7f394e6587cfffd4148e96a3694
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-05-25 15:40:38 +0100

    Add Bikeyear to display on the list

--- src/components/BikesList.js
+++ src/components/BikesList.js
@@ -21,7 +21,7 @@ const BikesList = () => {
       <table className="table-auto w-4/5 max-w-xl mx-auto text-sm md:text-lg">
         <thead className="">
           <tr>
-            <th className="p-2 text-left">Bike make</th>
+            <th className="p-2 text-left">Bike Year</th>
             <th className="p-2 text-left">Bike model</th>
             <th className="p-2 text-left">Action</th>
           </tr>
@@ -30,7 +30,7 @@ const BikesList = () => {
         <tbody>
           {bikes.map((bike) => (
             <tr key={bike.id}>
-              <td className="px-2 py-4">{bike.make}</td>
+              <td className="px-2 py-4">{bike.year}</td>
               <td className="px-2 py-4">{bike.model}</td>
               <td className="px-2 py-4">
                 <PrimaryButton onClick={() => handleDelete(bike.id)}>
