ff4960907bdf000aed7f01c325fd0021c5eeafb9
cwaku <rickymormor1@gmail.com>
2022-05-25 08:55:09 +0000

    fetch a single scooter

--- src/components/reserve.js
+++ src/components/reserve.js
@@ -1,11 +1,36 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { useSelector } from 'react-redux';
-import { selectAvailableScooterIds, useGetAvailableScotersQuery } from '../redux/reservations/reservationsSlice';
+import {
+  selectAvailableScooterIds,
+  // selectAllReservation,
+  useGetAvailableScotersQuery,
+  // useGetReservationQuery,
+  // extendedReservationsSlice,
+} from '../redux/reservations/reservationsSlice';
+// import store from "../redux/configureStore";
 
 const Reserve = () => {
   const { isLoading, isSuccess, isError } = useGetAvailableScotersQuery();
+  // const {
+  //   isLoading: isLoadingReservation,
+  //   isSuccess: isSuccessReservation,
+  //   isError: isErrorReservation,
+  // } = useGetReservationQuery();
 
   const orderedScootersIds = useSelector(selectAvailableScooterIds);
+  // const reservation = useSelector(selectAllReservation);
   // const scootersStatus = useSelector(getAvailableScootersStatus);
+
+  const getId = (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    // store.dispatch(
+    //   extendedReservationsSlice.endpoints.getReservation.initiate({
+    //     id: e.target.id,
+    //   }),
+    // );
+  };
 
   let content = null;
   if (isLoading) {
@@ -15,7 +40,11 @@ const Reserve = () => {
     content = <p>Error!</p>;
   }
   if (isSuccess) {
-    content = orderedScootersIds.map((id) => <p key={id}>{id}</p>);
+    content = orderedScootersIds.map((id) => (
+      <p onClick={getId} key={id} id={id}>
+        {id}
+      </p>
+    ));
   }
 
   return <div>{content}</div>;
--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -30,19 +30,46 @@ const extendedReservationsSlice = reservationsApi.injectEndpoints({
       },
       // add providesTags when needed
     }),
+    getReservation: builder.query({
+      query: (id) => `/reserve_motorcycle/:id/${id}`, // change endpoint to scooters when database is updated
+      transformResponse: (responseData) => {
+        const loadedScooter = responseData.map((scooter) => {
+          if (!scooter?.country) scooter.country = 'N/A';
+          if (!scooter?.make) scooter.make = 'N/A';
+
+          return scooter;
+        });
+
+        return reservationsAdapter.setAll(
+          initialState,
+          loadedScooter,
+        ); /* normalize data
+        (entity object with a bunch of objects in it.
+          we use that ids array to look up the objects in the state) */
+      },
+      // add providesTags when needed
+    }),
   }),
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export const { useGetAvailableScotersQuery } = extendedReservationsSlice;
+export const { useGetAvailableScotersQuery, useGetReservationQuery } = extendedReservationsSlice;
 
 // returns the query result object
 export const selectAvailableScootersResult = extendedReservationsSlice
   .endpoints.getAvailableScoters.select();
 
+export const selectReservationResult = extendedReservationsSlice
+  .endpoints.getReservation.select();
+
 //  Create memoized selector
 export const selectAvailableScootersData = createSelector(
   [selectAvailableScootersResult],
+  (result) => result.data, // Normalized state with ids and entities
+);
+
+export const selectReservationData = createSelector(
+  [selectReservationResult],
   (result) => result.data, // Normalized state with ids and entities
 );
 
@@ -52,5 +79,10 @@ export const {
   selectById: selectAvailableScooterById,
   selectIds: selectAvailableScooterIds,
 } = reservationsAdapter.getSelectors((state) => selectAvailableScootersData(state) ?? initialState);
+
+export const {
+  selectAll: selectAllReservation,
+  selectById: selectReservationById,
+} = reservationsAdapter.getSelectors((state) => selectReservationData(state) ?? initialState);
 
 export default extendedReservationsSlice;
