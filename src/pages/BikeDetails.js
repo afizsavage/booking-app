79cac3640e6d943dc9c7d30c2f5687e67d89371a
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-05-26 13:14:52 +0100

    Add styles to the details page

--- src/pages/BikeDetails.js
+++ src/pages/BikeDetails.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BsFillGearFill, BsArrowRightCircle } from 'react-icons/bs';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -24,53 +25,62 @@ const BikeDetails = () => {
   }
 
   return (
-    <div className="flex flex-col gap-[30%] p-2 items-center justify-center md:w-4/5">
+    <div className="flex flex-col gap-[30%] p-0 items-stretch justify-center md:w-4/5">
       <div key={bike.id} className="flex flex-col gap-[30%]">
-        <div className="flex  justify-center  w-full">
-          <h1 className="font-semibold text-2xl">
-            {' '}
-            <span>
-              {' '}
-              {bike.make}
-            </span>
-            <br />
-            <span>{bike.model}</span>
-            <span>
-              {' '}
-            </span>
-            {' '}
-          </h1>
-        </div>
-
         <div className="flex flex-col  gap-[10%] items-center md:flex-row md:gap-[10%] md:items-center">
-          <div className="bg-amber-500 w-80 h-80 rounded-full mx-auto relative mb-4 flex justify-center items-center content-center md:bg-amber-500 md:w-[30rem] md:h-[15rem] md:rounded-full ">
+          <div className="bg-amber-500 w-80 h-80 rounded-full mx-auto relative mb-4 flex sm:flex-row justify-between items-center content-center md:bg-amber-500 md:w-[30rem] md:h-[15rem] md:rounded-full ">
             <div className="flex justify-center items-center content-center">
               <img className="w-[100%] h-[100%] md:w-[100%] md:h-[100%]" src={`https://sheltered-tor-84017.herokuapp.com${bike.image.url}`} alt="img" />
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center gap-4">
-            <div className="w-[70%] ">
-              <p>
-                Title:
-                {bike.title}
-              </p>
+          <div className="flex flex-col justify-center pl-24 items-center gap-4">
+            <div className="flex  justify-center  w-full">
+              <h1 className="font-semibold pr-10 text-2xl">
+                {' '}
+                <span>
+                  {' '}
+                  {bike.make}
+                </span>
+                <br />
+                <span>{bike.model}</span>
+                <span>
+                  {' '}
+                </span>
+                {' '}
+              </h1>
             </div>
-            <div className="w-[70%] ">
-              <p>
-                Year:
-                {bike.year}
-              </p>
-            </div>
-            <p className="w-[70%] ">
+            <p className="w-[100%] ">
               {' '}
               {bike.description}
               {' '}
             </p>
+            <div className="w-[100%] ">
+              <p className="bg-gray-200 p-2 flex justify-between">
+                <span>Name:</span>
+                {bike.title}
+              </p>
+            </div>
+            <div className="w-[100%] ">
+              <p className="flex justify-between">
+                <span>Year:</span>
+                {bike.year}
+              </p>
+            </div>
+            <div className="w-[100%] ">
+              <p className="bg-gray-200 p-2 flex justify-between">
+                <span>color:</span>
+                {bike.color}
+              </p>
+            </div>
             <div>
               <Link to="/reserve">
-                <button type="button" className="bg-amber-500    hover:bg-amber-600/80 text-white font-bold py-2 px-4 rounded w-52">
-                  RESERVE
-                </button>
+                <div className="flex bg-amber-500    hover:bg-amber-600/80 text-white font-bold py-1 px-4 rounded w-42">
+                  <BsFillGearFill className="mx-2" size={30} color="white" />
+                  <button type="button" className="">
+                    RESERVE
+                  </button>
+                  <BsArrowRightCircle className="mx-2" size={30} color="white" />
+                </div>
               </Link>
             </div>
             <span />
